fb93bb8092874b7e52add66d88a16b75b119a426
Alfredo Canziani <alfredo.canziani@gmail.com>
2019-01-20 19:43:19 -0500

    Clean up a lil

--- get_AMC_movies.py
+++ get_AMC_movies.py
@@ -1,17 +1,11 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+URL = 'https://www.amctheatres.com/movies'
+
 
 def get_AMC_movies():
-    # AMC URL
-    URL = 'https://www.amctheatres.com/movies'
-
-    # Open the AMC page
     conn = urlopen(URL)
-    # reply = conn.read()
-    # print(reply)
-
-    # Get the movies
     soup = BeautifulSoup(conn, 'html.parser')
     movies = soup.find_all(name='div', class_='MoviePostersGrid-poster')
     titles = tuple(m.find(name='a')['aria-label'] for m in movies)
@@ -19,5 +13,5 @@ def get_AMC_movies():
 
 
 if __name__ == '__main__':
-    print(' > Movies playing at AMC theatres')
+    print(' > Top-5 movies playing at AMC theatres')
     print(*get_AMC_movies()[:5], sep='\n')
